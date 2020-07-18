0314f98bb2be4853eee96a8b73796af125f19810
Robbert Vermeulen <robbertvermeulen@gmail.com>
2020-07-18 15:44:53 +0200

    Implement glidejs and carousel styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
 jQuery(document).ready(function($) {
 
+   const glide = new Glide('.glide', {
+      type: 'carousel',
+      perView: 4,
+      gap: 20,
+      breakpoints: {
+         800: {
+            perView: 3
+         },
+         600: {
+            perView: 1
+         }
+      }
+   });
+   glide.mount();
+
    $('.js-hamburger').click(function() {
       $(this).toggleClass('is-open');
       $('.js-header-nav').toggleClass('is-open');
    });
+
+
 
 });
